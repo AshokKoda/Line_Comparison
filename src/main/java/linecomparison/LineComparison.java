fd7cab10f59c90ab27424dc099eaef49364702a0
Ashok Koda <kodaashok60@gmail.com>
2022-02-17 16:54:38 +0530

    Added:Equality of two lengths

--- src/main/java/linecomparison/LineComparison.java
+++ src/main/java/linecomparison/LineComparison.java
@@ -3,7 +3,7 @@ package linecomparison;
 import java.util.Scanner;
 
 public class LineComparison {
-	
+
 	public static int getInputFromUser(String number) {
 		System.out.println("Enter the " + number + " value");
 		Scanner sc = new Scanner(System.in);
@@ -12,18 +12,31 @@ public class LineComparison {
 
 	public static void main(String[] args) {
 		System.out.println("-----------Welcome to Line Comparison Computation-----------");
-		
-		//UC1 - Calculate Length
+
+		// UC1 - Calculate Length
 		System.out.println("Enter the first (x1, y1) point");
 		int x1 = getInputFromUser("x1");
-        int y1 = getInputFromUser("y1");
-        System.out.println("Enter the second (x2, y2) point");
-        int x2 = getInputFromUser("x2");
-        int y2 = getInputFromUser("y2");
-        double x = Math.pow((x2 - x1), 2);
-        double y = Math.pow((y2 - y1), 2);
-        Double length = Math.floor(Math.sqrt(x + y));
-        System.out.println("Length is: " + length);
+		int y1 = getInputFromUser("y1");
+		System.out.println("Enter the second (x2, y2) point");
+		int x2 = getInputFromUser("x2");
+		int y2 = getInputFromUser("y2");
+		double x = Math.pow((x2 - x1), 2);
+		double y = Math.pow((y2 - y1), 2);
+		Double length = Math.floor(Math.sqrt(x + y));
+		System.out.println("Length is: " + length);
+		
+		//UC2 - Equality of two lines
+		System.out.println("Enter the second line of co-ordinated");
+        System.out.println("Enter the first (a1, b1) point");
+        int a1 = getInputFromUser("a1");
+        int b1 = getInputFromUser("b1");
+        System.out.println("Enter the second (a2, b2) point");
+        int a2 = getInputFromUser("a2");
+        int b2 = getInputFromUser("b2");
+        double a = Math.pow((a2 - a1), 2);
+        double b = Math.pow((b2 - b1), 2);
+        double length1 = Math.floor(Math.sqrt(a + b));
+        System.out.println("Length is: " + length1);
 
 	}
 
